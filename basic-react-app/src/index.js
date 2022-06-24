39522818fe2ece0e1735d368115bf6407b774361
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-24 17:58:20 -0400

    - Render Props

--- basic-react-app/src/index.js
+++ basic-react-app/src/index.js
@@ -377,6 +377,40 @@ return(
   </form>
 );
 }
+
+//Working with render props
+const tahoe_peaks = [
+  {name: "Freel", elevation: 10891},
+  {name: "Monument", elevation: 10067 },
+  {name: "Pyramid", elevation: 9983},
+  {name: "Tallac", elevation: 9735}
+];
+
+function List({data, renderItem, renderEmpty}){
+  return !data.length ? (
+    renderEmpty
+  ): (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function RenderPropsDemo(){
+ return(
+   <List 
+   data={tahoe_peaks} 
+   renderEmpty={<p>This is the list is empty</p>}
+   renderItem={(item) => (<>
+   {item.name} - {item.elevation} ft.
+    </>)}/>
+ );
+}
+
 root.render(
   // React.createElement("div", {style:{color:"blue"}}, React.createElement("h1", null, "Hello!"))
   <div>
@@ -394,6 +428,7 @@ root.render(
   <CheckBox2/>
   <FormUnControlledComponent/>
   <FormControlledComponent/>
+  <RenderPropsDemo />
   </div>
 );
 
